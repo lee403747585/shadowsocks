cec65108bb26d02329e7900eaf351c1acfe4ee68
lee403747585 <30380784+lee403747585@users.noreply.github.com>
2019-01-23 16:29:35 +0800

    Add files via upload

---
+++ userapiconfig.py
@@ -0,0 +1,47 @@
+# Config
+NODE_ID = 157
+
+
+# hour,set 0 to disable
+SPEEDTEST = 6
+CLOUDSAFE = 0
+ANTISSATTACK = 0
+AUTOEXEC = 0
+
+MU_SUFFIX = 'windows.com'
+MU_REGEX = '%5m'
+
+SERVER_PUB_ADDR = '127.0.0.1'  # mujson_mgr need this to generate ssr link
+API_INTERFACE = 'modwebapi'  # glzjinmod, modwebapi
+
+WEBAPI_URL = 'https://12.345cxz.top'
+WEBAPI_TOKEN = 'cmxx'
+
+# mudb
+MUDB_FILE = 'mudb.json'
+
+# Mysql
+MYSQL_HOST = '127.0.0.1'
+MYSQL_PORT = 3306
+MYSQL_USER = 'ss'
+MYSQL_PASS = 'ss'
+MYSQL_DB = 'shadowsocks'
+
+MYSQL_SSL_ENABLE = 0
+MYSQL_SSL_CA = ''
+MYSQL_SSL_CERT = ''
+MYSQL_SSL_KEY = ''
+
+# API
+API_HOST = '127.0.0.1'
+API_PORT = 80
+API_PATH = '/mu/v2/'
+API_TOKEN = 'abcdef'
+API_UPDATE_TIME = 60
+
+# Manager (ignore this)
+MANAGE_PASS = 'ss233333333'
+# if you want manage in other server you should set this value to global ip
+MANAGE_BIND_IP = '127.0.0.1'
+# make sure this port is idle
+MANAGE_PORT = 23333
