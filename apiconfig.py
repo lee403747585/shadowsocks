d315963577b64938daee7f597a68a2532bd0140d
lee403747585 <30380784+lee403747585@users.noreply.github.com>
2019-01-24 20:50:24 +0800

    Update apiconfig.py

--- apiconfig.py
+++ apiconfig.py
@@ -1,4 +1,4 @@
-﻿# Config
+# Config
 NODE_ID = 1
 
 
@@ -12,7 +12,7 @@ MU_SUFFIX = 'zhaoj.in'
 MU_REGEX = '%5m%id.%suffix'
 
 SERVER_PUB_ADDR = '127.0.0.1'  # mujson_mgr need this to generate ssr link
-API_INTERFACE = 'glzjinmod'  # glzjinmod, modwebapi
+API_INTERFACE = 'modwebapi'  # glzjinmod, modwebapi
 
 WEBAPI_URL = 'https://zhaoj.in'
 WEBAPI_TOKEN = 'glzjin'
